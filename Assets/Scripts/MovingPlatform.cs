4eb976692e5ba513eabd46f84be4523f14fe7951
ChimgoHyperion <ectowaregames@gmail.com>
2025-03-07 09:16:45 +0100

    March 7th commit (Added up to Level 9)

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,43 +4,138 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    //public Transform up;
+    //public Transform down;
+
+
+    //float upLimit;
+    //float downLimit;
+    //public float speed;
+    //private int direction = 1;
+    //private Vector2 upDownmovement;
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    upLimit = up.transform.position.y;
+    //    downLimit = down.transform.position.y;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    if (transform.position.y > upLimit)
+    //    {
+    //        direction = -1;
+    //    }
+    //    else if (transform.position.y < downLimit)
+    //    {
+    //        direction = 1;
+    //    }
+    //    upDownmovement = Vector2.up * direction * speed * Time.deltaTime;
+
+    //    transform.Translate(upDownmovement);
+
+
+    //}
+
+
+
+    public enum MovementType
+    {
+        Vertical,
+        Horizontal,
+        Both
+    }
+
+    public MovementType movementType = MovementType.Both;
+
+    [Header("Vertical Boundaries (Used for Vertical & Both)")]
     public Transform up;
     public Transform down;
 
+    [Header("Horizontal Boundaries (Used for Horizontal & Both)")]
+    public Transform left;
+    public Transform right;
 
-    float upLimit;
-    float downLimit;
-    public float speed;
-    private int direction = 1;
-    private Vector2 upDownmovement;
-    
-    
-    // Start is called before the first frame update
+    public float speed = 2f;
+
+    private float upLimit;
+    private float downLimit;
+    private float leftLimit;
+    private float rightLimit;
+
+    // Direction multipliers
+    private int verticalDirection = 1;   // 1 = moving up, -1 = moving down
+    private int horizontalDirection = 1; // 1 = moving right, -1 = moving left
+
     void Start()
     {
-        upLimit = up.transform.position.y;
-        downLimit = down.transform.position.y;
+        // Set boundaries if needed based on the movement type
+        if (movementType == MovementType.Vertical || movementType == MovementType.Both)
+        {
+            if (up != null && down != null)
+            {
+                upLimit = up.position.y;
+                downLimit = down.position.y;
+            }
+            else
+            {
+                Debug.LogWarning("Vertical boundaries are not assigned!");
+            }
+        }
+
+        if (movementType == MovementType.Horizontal || movementType == MovementType.Both)
+        {
+            if (left != null && right != null)
+            {
+                leftLimit = left.position.x;
+                rightLimit = right.position.x;
+            }
+            else
+            {
+                Debug.LogWarning("Horizontal boundaries are not assigned!");
+            }
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 movement = Vector2.zero;
 
-        if (transform.position.y > upLimit)
+        // Vertical movement
+        if (movementType == MovementType.Vertical || movementType == MovementType.Both)
         {
-            direction = -1;
+            if (transform.position.y > upLimit)
+            {
+                verticalDirection = -1;
+            }
+            else if (transform.position.y < downLimit)
+            {
+                verticalDirection = 1;
+            }
+            movement.y = verticalDirection;
         }
-        else if (transform.position.y < downLimit)
+
+        // Horizontal movement
+        if (movementType == MovementType.Horizontal || movementType == MovementType.Both)
         {
-            direction = 1;
+            if (transform.position.x > rightLimit)
+            {
+                horizontalDirection = -1;
+            }
+            else if (transform.position.x < leftLimit)
+            {
+                horizontalDirection = 1;
+            }
+            movement.x = horizontalDirection;
         }
-        upDownmovement = Vector2.up * direction * speed * Time.deltaTime;
 
-        transform.Translate(upDownmovement); 
-
-       
+        // Apply movement
+        transform.Translate(movement * speed * Time.deltaTime);
     }
-   
 
+} 
 
-}
