b532afe1883ba681b011100c0ec751d2d8dc0edb
ChimgoHyperion <ectowaregames@gmail.com>
2025-03-08 20:59:53 +0100

    fixed buying skin mismatch bug

--- Assets/FireBallCol.cs
+++ Assets/FireBallCol.cs
@@ -37,9 +37,9 @@ public class FireBallCol : MonoBehaviour
             }
 
         }
-        if (collision.gameObject.name=="Obstacle") // temporary fix
-        {
-            Destroy(collision.gameObject);
-        }
+        //if (collision.gameObject.name=="Obstacle") // temporary fix
+        //{
+        //    Destroy(collision.gameObject);
+        //}
     }
 }
